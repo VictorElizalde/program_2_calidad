47f2d19f2720be833c4a3343ec1a457b257fc00f
Victor Elizalde <vic-bull@hotmail.com>
2017-02-13 16:13:27 -0600

    Erase items not applicable

--- Part.h
+++ Part.h
@@ -43,7 +43,6 @@ class Part { //&m
 
 };
 
-//&i
 Part :: Part() //&m
 {
   sName = "N/A";
@@ -56,7 +55,6 @@ Part :: Part() //&m
   iTotal = 0;
 }
 
-//&i
 Part :: Part(string sName)
 {
   this -> sName = sName;
@@ -69,97 +67,81 @@ Part :: Part(string sName)
   iTotal = 0;
 }
 
-//&i
 void Part :: setName(string sName) //&m
 {
   this -> sName = sName;
 }
 
-//&i
 void Part :: setType(string sType) //&m
 {
   this -> sType = sType; //&m
 }
 
-//&i
 void Part :: setItems(int iItems) //&m
 {
   this -> iItems = iItems; //&m
 }
 
-//&i
 void Part :: setBase(int iBase)
 {
   this -> iBase = iBase;
 }
 
-//&i
 void Part :: setMod(int iMod)
 {
   this -> iMod = iMod;
 }
 
-//&i
 void Part :: setDel(int iDel)
 {
   this -> iDel = iDel;
 }
 
-//&i
 void Part :: setAdded(int iAdded)
 {
   this -> iAdded = iAdded;
 }
 
-//&i
 void Part :: setTotal(int iTotal)
 {
   this -> iTotal = iTotal;
 }
 
-//&i
 string Part :: getName() //&m
 {
   return sName;
 }
 
-//&i
 string Part :: getType() //&m
 {
   return sType; //&m
 }
 
-//&i
 int Part :: getItems()
 {
   return iItems;
 }
 
-//&i
 int Part :: getBase()
 {
   return iBase;
 }
 
-//&i
 int Part :: getMod()
 {
   return iMod;
 }
 
-//&i
 int Part :: getDel()
 {
   return iDel;
 }
 
-//&i
 int Part :: getAdded()
 {
   return iAdded;
 }
 
-//&i
 int Part :: getTotal()
 {
   return iTotal;
